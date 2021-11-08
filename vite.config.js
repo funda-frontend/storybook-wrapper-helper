a7b65cf858943fcbe3ef98323b01d95cfb02fdbc
Connor Christensen <connor@funda.nl>
2021-11-08 11:20:02 +0100

    feat: generate type definition files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+    plugins: [dts()],
     build: {
         lib: {
             entry: path.resolve(__dirname, 'src/index.ts'),
             name: 'StorybookWrapper',
             fileName: (format) => `storybook-wrapper.${format}.js`,
-        }
+        },
     },
 });
